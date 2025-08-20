34f13a5510d48263accc8d15886b79b363944a6f
Rohan <rohansingh8382@gmail.com>
2025-08-20 12:25:10 +0530

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,9 @@ const User = require('../models/user');
 
 exports.getProfile = async (req, res) => {
 try {
-    let params = req.query.id
-const user = await User.findOne({_id:"68a56782ed43373e367d84fb"});
+    let params = req.params.id
+    console.log(req)
+const user = await User.findOne({_id:params});
 if (!user) return res.status(404).json({ error: 'User not found' });
 return res.status(200).send({status: true, data: user})
 } catch (err) {
